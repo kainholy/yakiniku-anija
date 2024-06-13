36cb9e343fd062a8b07da4c9ea134a847cdc1f0d
kainholy <ookuraseiya0506@gmail.com>
2024-06-13 21:20:56 +0900

    修正

--- src/components/sections/menu/List.tsx
+++ src/components/sections/menu/List.tsx
@@ -1,7 +1,7 @@
 // components/sections/menu/MenuList.tsx
 import { FC } from "react";
 import { Menu } from "../../../../types/menu";
-const base = "p-top-menu";
+const base = "p-menu-list";
 
 interface MenuListProps {
   menulist: Menu[];
@@ -12,56 +12,59 @@ const MenuList: FC<MenuListProps> = ({ menulist }) => {
     <section className={base}>
       <div className={base + "__container"}>
         <div className={base + "__content-area"}>
-          <h1>メニュー</h1>
 
           {menulist.map((menu) => (
-            <div key={menu.id}>
-              <h2 className={base + "__item__name c-top-menu-name"}>
+            <div key={menu.id} className={base+'__category'}>
+              <h2 className={base + "__item__title c-menu-category-name"}>
                 {menu.title}
               </h2>
 
-              <ul className={base + "__list"}>
-                {menu.menu.map((item) => (
-                  <li className={base + "__item"} key={item.title}>
-                    <div>
-                      <div>
+              {menu.menu && (
+                <ul className={base + "__list"}>
+                  {menu.menu.map((item) => (
+                    <li className={base + "__item"} key={item.title}>
                         {item.image && (
-                          <img
-                            className={base + "__item__img"}
-                            src={item.image.url}
-                            alt={item.title}
-                          />
+                          <div className={base + "__item__img-area"}>
+                            <img
+                              className={base + "__item__img"}
+                              src={item.image.url}
+                              alt={item.title}
+                            />
+                          </div>
                         )}
-                      </div>
-                      <p className={base + "__item__name c-top-menu-name"}>
+                        <p className={base + "__item__name c-top-menu-name"}>
+                          {item.title}
+                        </p>
+                        <p className={base + "__item__price c-top-menu-price"}>
+                          {item.price}円
+                        </p>
+                        <p className={base + "__item__price c-top-menu-price"}>
+                          （税込{Math.round(item.price * 1.1)}円）
+                        </p>
+                    </li>
+                  ))}
+                </ul>
+              )}
+
+              {menu.menu_noeye && (
+                <ul className={base + "__sub-menu__list"}>
+                  {menu.menu_noeye.map((item) => (
+                    <li key={item.title} className={base+'__sub-menu__item'}>
+                      <p className={base + "__sub-menu__item__name c-top-menu-name"}>
                         {item.title}
                       </p>
-                      <p className={base + "__item__price c-top-menu-price"}>
-                        {item.price}円
-                      </p>
-                      <p className={base + "__item__price c-top-menu-price"}>
-                        （税込{Math.round(item.price * 1.1)}円）
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-
-              <ul>
-                {menu.menu_noeye.map((item) => (
-                  <li key={item.title}>
-                    <p className={base + "__item__name c-top-menu-name"}>
-                      {item.title}
-                    </p>
-                    <p className={base + "__item__price c-top-menu-price"}>
-                      {item.price}円
-                    </p>
-                    <p className={base + "__item__price c-top-menu-price"}>
-                      （税込{Math.round(item.price * 1.1)}円）
-                    </p>
-                  </li>
-                ))}
-              </ul>
+                      <div className={base+'__sub-menu__item__price-area'}>
+                        <p className={base + "__sub-menu__item__price c-top-menu-price"}>
+                          {item.price}円
+                        </p>
+                        <p className={base + "__sub-menu__item__price c-top-menu-price"}>
+                          （税込{Math.round(item.price * 1.1)}円）
+                        </p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
           ))}
         </div>
