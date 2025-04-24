05559361cdaf091d7104b068a3a0ae66f3121c26
kainholy <ookuraseiya0506@gmail.com>
2025-04-24 17:05:09 +0900

    output exportに変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+    formats: ['image/avif'],
+    domains: ['images.microcms-assets.io'], // Add the domain serving your images
+  },
 };
 
 export default nextConfig;
