926c9d2a6cfa7ecf7d8a119196602bc3e2e76904
kainholy <ookuraseiya0506@gmail.com>
2025-04-24 18:31:58 +0900

    修正

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    formats: ['image/avif'],
-    domains: ['images.microcms-assets.io'], // Add the domain serving your images
   },
 };
 
